14861a5d16e74d0f9b34aa5f40c63f5b75bc1e3e
watripont <jeremy.watripont@etu.univ-amu.fr>
2025-01-28 16:31:08 +0100

    Commit du TestCircle.java , ajout de "et il est de couleur " + c1.getColor()

--- src/TestCircle.java
+++ src/TestCircle.java
@@ -3,9 +3,9 @@ public class TestCircle {
    public static void main(String[] args) {
       Circle c1 = new Circle();
       System.out.println("le cercle a un rayon de " 
-         + c1.getRadius() + " et une superficie de " + c1.getArea());
+         + c1.getRadius() + " et une superficie de " + c1.getArea() + "et il est de couleur " + c1.getColor());
       Circle c2 = new Circle(2.0);
       System.out.println("le cercle a un rayon de " 
-         + c2.getRadius() + " et une superficie de " + c2.getArea());
+         + c2.getRadius() + " et une superficie de " + c2.getArea() + "et il est de couleur " + c2.getColor());
    }
 }
