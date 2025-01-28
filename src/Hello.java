5e8f59e87d643d8d5981fc76392971de62cc3c80
watripont <jeremy.watripont@etu.univ-amu.fr>
2025-01-28 14:57:57 +0100

    Premiere modification

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
    public static void main(String[] args) {
-      System.out.println("hello, world, ceci est mon 1er prog");
+      System.out.println("hello, world, ceci est mon 1er prog et ma 1ere modification");
    }
 }
