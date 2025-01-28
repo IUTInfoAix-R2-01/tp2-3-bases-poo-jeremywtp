e8b3c780830cf8b34cd081653c681e7083b357de
watripont <jeremy.watripont@etu.univ-amu.fr>
2025-01-28 16:48:42 +0100

    Commit de TestCircle.java ajout de texte pour décrire la situation de changement de rayon

--- src/TestCircle.java
+++ src/TestCircle.java
@@ -7,8 +7,8 @@ public class TestCircle {
       Circle c2 = new Circle(2.0);
       System.out.println("le cercle a un rayon de " 
          + c2.getRadius() + " et une superficie de " + c2.getArea() + " et il est de couleur " + c2.getColor());
-      System.out.println(c1.getRadius());
+      System.out.println("le cercle a un rayon de " + c1.getRadius());
       c1.setRadius(5.0); 
-      System.out.println(c1.getRadius());
+      System.out.println("le cercle a maintenant un nouveau rayon de " + c1.getRadius());
    }
 }
